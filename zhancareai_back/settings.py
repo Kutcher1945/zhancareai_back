84b3111c47493535669fa799fc06dc81df5daec4
Adilan <adilan.akhramovich@gmail.com>
2025-02-26 08:46:32 +0500

    added a new functions

--- zhancareai_back/settings.py
+++ zhancareai_back/settings.py
@@ -120,13 +120,23 @@ WSGI_APPLICATION = "zhancareai_back.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.dminzprubquadrtwmoxr",
+        "PASSWORD": "4HPzQt2HyU@",  # Replace with your actual password
+        "HOST": "aws-0-eu-central-1.pooler.supabase.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
